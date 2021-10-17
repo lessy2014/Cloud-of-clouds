6dde7fa0ea11326a1e3aa0722dc8005872ca2c37
lessy2014 <lessik03@gmail.com>
2021-10-17 22:22:48 +0500

    Can handle multiple tokens

--- COC/COC/Program.cs
+++ COC/COC/Program.cs
@@ -18,21 +18,29 @@ namespace COC
     {
         static void Main(string[] args)
         {
-            //string token = "KRvzqGRO6vAAAAAAAAAAAc2uCm6q-u2hLD8e-IhvyDrRIX6FESDBqyp90dY31HvU";
-            //var a = new FrmDropBoxWhtverThatMeans(token);
-            // a.GetFolderData("");
-            // Console.WriteLine();
-            // a.GetFolderData(@"/Folder1");
-            // Console.WriteLine();
-            // a.GetFolderData(@"/Folder1/FolderInsideFolder1");
-            var b = new FrmDropBoxWhtverThatMeans();
-            b.GetFolderData();
+            string token1 = "KRvzqGRO6vAAAAAAAAAAAc2uCm6q-u2hLD8e-IhvyDrRIX6FESDBqyp90dY31HvU";
+            string token2 = "kjAhtJMFcp4AAAAAAAAAAX_wzLndk7IlBAYVeYs0yLmkq_d_0Kt_wdQm5lvVVmNJ";
+            var a = new FrmDropBoxWhtverThatMeans(token1);
+            a.AddToken(token2);
+            a.GetFolderData("");
+            a.GetFolderData("sigmarblessme@gmail.com");
+            a.GetFolderData("sigmarblessme@gmail.com/Folder1");
+             // var b = new FrmDropBoxWhtverThatMeans(token2);
+             // b.GetFolderData("");
+             // Console.WriteLine();
+             // a.GetFolderData(@"/Folder1");
+             // Console.WriteLine();
+             // a.GetFolderData(@"/Folder1/FolderInsideFolder1");
+             // var b = new FrmDropBoxWhtverThatMeans();
+             // b.GetFolderData();
         }
     }
 
     public class FrmDropBoxWhtverThatMeans
     {
-         private string token;
+         private string currentToken;
+         private List<string> tokens;
+         private Dictionary<string, string> mailsToTokens;
          private string name;
          private string email;
 
@@ -41,68 +49,76 @@ namespace COC
          private List<Metadata> currentFolder;
          private string folder;
 
-         public FrmDropBoxWhtverThatMeans(string token="")
+         public FrmDropBoxWhtverThatMeans(string token)
          {
-             this.token = token;
+             tokens = new List<string>();
+             currentToken = token;
+             tokens.Add(token);
          }
 
          public FrmDropBoxWhtverThatMeans()
+         { 
+             tokens = new List<string>();
+             AddToken();
+         }
+
+         public void AddToken()
          {
              Chilkat.OAuth2 oauth2 = new Chilkat.OAuth2();
              
-            oauth2.ListenPort = 3017;
+             oauth2.ListenPort = 3017;
             
+         
+             oauth2.AuthorizationEndpoint = "https://www.dropbox.com/oauth2/authorize";
+             oauth2.TokenEndpoint = "https://api.dropboxapi.com/oauth2/token";
+            
+             oauth2.ClientId = "ryj3w3kellyrapb";
+             oauth2.ClientSecret = "1ukvw813zamkzla";
+             oauth2.CodeChallenge = false;
+            
+             string url = oauth2.StartAuth();
+             if (oauth2.LastMethodSuccess != true) {
+                 Console.WriteLine(oauth2.LastErrorText);
+                 return;
+             }
+            
+             System.Diagnostics.Process.Start(url);
+             int numMsWaited = 0;
+             while ((numMsWaited < 30000) && (oauth2.AuthFlowState < 3)) {
+                 oauth2.SleepMs(100);
+                 numMsWaited = numMsWaited + 100;
+             }
+            
+             if (oauth2.AuthFlowState < 3) {
+                 oauth2.Cancel();
+                 Console.WriteLine("No response from the browser!");
+                 return;
+             }
+            
+             if (oauth2.AuthFlowState == 5) {
+                 Console.WriteLine("OAuth2 failed to complete.");
+                 Console.WriteLine(oauth2.FailureInfo);
+                 return;
+             }
+         
+             if (oauth2.AuthFlowState == 4) {
+                 Console.WriteLine("OAuth2 authorization was denied.");
+                 Console.WriteLine(oauth2.AccessTokenResponse);
+                 return;
+             }
+         
+             if (oauth2.AuthFlowState != 3) {
+                 Console.WriteLine("Unexpected AuthFlowState:" + Convert.ToString(oauth2.AuthFlowState));
+                 return;
+             }
+             currentToken = oauth2.AccessToken;
+             tokens.Add(currentToken);
+         }
 
-            oauth2.AuthorizationEndpoint = "https://www.dropbox.com/oauth2/authorize";
-            oauth2.TokenEndpoint = "https://api.dropboxapi.com/oauth2/token";
-            
-            oauth2.ClientId = "ryj3w3kellyrapb";
-            oauth2.ClientSecret = "1ukvw813zamkzla";
-            oauth2.CodeChallenge = false;
-            
-            string url = oauth2.StartAuth();
-            if (oauth2.LastMethodSuccess != true) {
-                Console.WriteLine(oauth2.LastErrorText);
-                return;
-            }
-            
-            System.Diagnostics.Process.Start(url);
-            int numMsWaited = 0;
-            while ((numMsWaited < 30000) && (oauth2.AuthFlowState < 3)) {
-                oauth2.SleepMs(100);
-                numMsWaited = numMsWaited + 100;
-            }
-            
-            if (oauth2.AuthFlowState < 3) {
-                oauth2.Cancel();
-                Console.WriteLine("No response from the browser!");
-                return;
-            }
-            
-            if (oauth2.AuthFlowState == 5) {
-                Console.WriteLine("OAuth2 failed to complete.");
-                Console.WriteLine(oauth2.FailureInfo);
-                return;
-            }
-
-            if (oauth2.AuthFlowState == 4) {
-                Console.WriteLine("OAuth2 authorization was denied.");
-                Console.WriteLine(oauth2.AccessTokenResponse);
-                return;
-            }
-
-            if (oauth2.AuthFlowState != 3) {
-                Console.WriteLine("Unexpected AuthFlowState:" + Convert.ToString(oauth2.AuthFlowState));
-                return;
-            }
-
-            // Console.WriteLine("OAuth2 authorization granted!");
-            //
-            // Console.WriteLine("Access Token Response = " + oauth2.AccessTokenResponse);
-            //
-            // Console.WriteLine("Access Token = " + oauth2.AccessToken);
-            token = oauth2.AccessToken;
-            // Console.WriteLine(token);
+         public void AddToken(string token)
+         {
+             tokens.Add(token);
+             currentToken = token;
          }
 
          public void GetFolderData(string folder="")
@@ -112,17 +128,18 @@ namespace COC
              try
              {
                  task.Wait();
-                 // Console.WriteLine(name);
-                 // Console.WriteLine(email);
-                 // Console.WriteLine(country);
-                 Console.Write("Current folder: ");
-                 Console.WriteLine(path);
-                 Console.WriteLine();
-                 foreach (var item in currentFolder)
+                 if (path != "")
                  {
-                     Console.WriteLine(item.Name);
+                     Console.Write("Current folder: ");
+                     Console.WriteLine(path);
+                     Console.WriteLine();
+                     foreach (var item in currentFolder)
+                     {
+                         Console.WriteLine(item.Name);
+                     }
                  }
-                 
+                 Console.WriteLine();
+
              }
              catch (Exception e)
              {
@@ -132,27 +149,55 @@ namespace COC
 
          public async Task Run()
         {
-            using (var dbc = new DropboxClient(token))
+            if (folder == "")
             {
-                var id = await dbc.Users.GetCurrentAccountAsync();
-                name = id.Name.DisplayName;
-                email = id.Email;
-                path = email + folder;
-                // country = id.Country;
-                try
-                {
-                    var list = await dbc.Files.ListFolderAsync(folder);
-                    currentFolder = new List<Metadata>();
-                    foreach (var item in list.Entries)
+                mailsToTokens = new Dictionary<string, string>();
+                path = "";
+                Console.WriteLine("Current folder: root");
+                Console.WriteLine();
+                foreach (var token in tokens)
+                    using (var dbc = new DropboxClient(token))
                     {
-                        currentFolder.Add(item);
+                        var id = await dbc.Users.GetCurrentAccountAsync();
+                        mailsToTokens.Add(id.Email, token);
+                        Console.WriteLine(id.Email);
+                    }
+            }
+            else
+            {
+                if (folder.Split('/').Length == 1)
+                {
+                    currentToken = mailsToTokens[folder];
+                    folder = "";
+                }
+                else if (folder.Split('/')[0].Length != 0)
+                {
+                    var length = folder.Split('/')[0].Length;
+                    currentToken = mailsToTokens[folder.Split('/')[0]];
+                    folder = folder.Remove(0, length);
+                }
+                using (var dbc = new DropboxClient(currentToken))
+                {
+                    var id = await dbc.Users.GetCurrentAccountAsync();
+                    name = id.Name.DisplayName;
+                    email = id.Email;
+                    path = email + folder;
+                    try
+                    {
+                        var list = await dbc.Files.ListFolderAsync(folder);
+                        currentFolder = new List<Metadata>();
+                        foreach (var item in list.Entries)
+                        {
+                            currentFolder.Add(item);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Whatever problem happened" + e);
                     }
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Whatever problem happened" + e);
-                }
             }
+            
         }
     }
 }
