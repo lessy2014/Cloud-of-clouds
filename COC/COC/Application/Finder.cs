8c7287051c4ad00139180fa35d053fed15e4cafe
SergeyBirukov <birukovsergej121@gmail.com>
2021-12-16 16:43:09 +0500

    Add filters

--- COC/COC/Application/Finder.cs
+++ COC/COC/Application/Finder.cs
@@ -1,26 +1,86 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using COC.ConsoleInterface;
 using COC.Infrastructure;
 
 namespace COC.Application
 {
     public class Finder
     {
-        public static void Find(Folder folder, string name)
+        public static void Find(Folder folder, string name, List<IFilter> filters, bool fullMatch)
         {
-            if (folder.Name.Contains(name))
+            try
             {
-                Console.WriteLine(folder.Path);
-            }
-            if (folder.Content.Count != 0)
-            {
+                if (name == "" && !IsHaveFilters(filters))
+                {
+                    Console.WriteLine("No name or filters specified");
+                    return;
+                }
+
+                if (SatisfiesConditions(folder, name, filters, fullMatch))
+                {
+                    Console.WriteLine(folder.Path);
+                }
+
+                if (folder.Content.Count == 0) return;
                 foreach (var fileSystemUnit in folder.Content.Values)
                 {
+                    if (SatisfiesConditions(fileSystemUnit, name, filters, fullMatch) &&
+                        fileSystemUnit.GetType() == typeof(File))
+                    {
+                        Console.WriteLine(fileSystemUnit.Path);
+                    }
+
                     if (fileSystemUnit is Folder unit)
                     {
-                        Find(unit, name);
+                        Find(unit, name, filters, fullMatch);
                     }
+
                 }
             }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Wrong type of object, only 'file' and 'folder' are supported");
+            }
+        }
+
+        private static bool SatisfiesConditions(IFileSystemUnit unit, string name, List<IFilter> filters, bool fullMatch)
+        {
+            if (!unit.Name.Contains(name) || (fullMatch && unit.Name != name))
+                return false;
+            foreach (var filter in filters)
+            {
+                if(filter is null)
+                    continue;
+                var filterType = filter.GetType();
+                if (filterType == typeof(ExtensionFilter) &&
+                    unit.Name.Split('.').Last() != filter.Value)
+                    return false;
+                if (filterType == typeof(TypeFilter))
+                {
+
+                    var t = GetTypeByName(filter.Value);
+                    if (unit.GetType() != t)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        private static Type GetTypeByName(string typeName)
+        {
+            return typeName.ToLower() switch
+            {
+                "folder" => typeof(Folder),
+                "file" => typeof(File),
+                _ => throw new ArgumentException()
+            };
+        }
+
+        private static bool IsHaveFilters(List<IFilter> filters)
+        {
+            return filters.Any(filter => filter != null);
         }
     }
 }
--- COC/COC/ConsoleInterface/FindCommand.cs
+++ COC/COC/ConsoleInterface/FindCommand.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Windows.Input;
 using COC.Application;
 using COC.Infrastructure;
@@ -9,8 +10,17 @@ namespace COC.ConsoleInterface
     [Verb("find", HelpText = "Find all files and folders with given name")]
     public class FindCommand: ICommand
     {
-        [Value(index:0, Required = true, HelpText = "Name of file or Folder")]
-        public string Name { get; set; }
+        [Value(index: 0, Required = false, HelpText = "Name of file or Folder")]
+        public string Name { get; set; } = "";
+
+        [Option("ext", Required = false, HelpText = "Search files only with the specified extension")]
+        public ExtensionFilter ExtensionFilter{ get; set; }
+
+        [Option('t', "type", Required = false, HelpText = "Type of object, supported arguments: 'folder' and 'file'")] 
+        public TypeFilter TypeFilter{ get; set; }
+
+        [Option("fm", Required = false, HelpText = "Show only names that completely match the request")]
+        public bool FullMatch { get; set; }
         
         public bool CanExecute(object parameter)
         {
@@ -19,9 +29,33 @@ namespace COC.ConsoleInterface
 
         public void Execute(object parameter)
         {
-            Finder.Find(FileSystemManager.CurrentFolder, Name);
+            Finder.Find(FileSystemManager.CurrentFolder, Name, new List<IFilter>{ExtensionFilter, TypeFilter}, FullMatch);
         }
 
         public event EventHandler CanExecuteChanged;
+    }
+
+    public interface IFilter
+    {
+        string Value { get; set; }
+    }
+
+    public class ExtensionFilter: IFilter
+    {
+        public string Value { get; set; }
+
+        public ExtensionFilter(string value)
+        {
+            Value = value;
+        }
+    } 
+    public class TypeFilter: IFilter
+    {
+        public string Value { get; set; }
+
+        public TypeFilter(string type)
+        {
+            Value = type;
+        }
     }
 }
